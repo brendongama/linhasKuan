d53ee8acee93c68ef3783f1eccc2049ac4b6bf07
brendongama <brendon_gama@hotmail.com>
2021-11-22 22:10:40 -0300

    Crud das linhas Kuan

---
+++ src/main/java/com/linhaskuan/repository/PedidosRepository.java
@@ -0,0 +1,13 @@
+package com.linhaskuan.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.linhaskuan.domain.Pedido;
+
+public interface PedidosRepository extends JpaRepository<Pedido, Long> {
+
+	Optional<Pedido> findByRg(String rg);
+
+}
